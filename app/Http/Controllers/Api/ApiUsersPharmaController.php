73681cd8f98c3e3d10000fbbc71c4a9e6638d7b7
theodoreyapi <theodoreyapi93@gmail.com>
2026-05-05 17:06:32 +0000

    update

--- app/Http/Controllers/Api/ApiUsersPharmaController.php
+++ app/Http/Controllers/Api/ApiUsersPharmaController.php
@@ -16,12 +16,10 @@ use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\ValidationException;
 
 class ApiUsersPharmaController extends Controller
 {
